f01545dd16e32447e400922ec268af6d990bf2dd
MakenaLow <makenal@stanford.edu>
2017-07-30 04:28:10 -0700

    Added timer

--- Hide-And-Seek/HiderViewController.swift
+++ Hide-And-Seek/HiderViewController.swift
@@ -10,9 +10,22 @@ import Foundation
 import UIKit
 
 class HiderViewController: UIViewController {
+   
+    @IBOutlet weak var timeLabel: UILabel!
+    
+    let formatter = DateFormatter()
+    let userCleander = Calendar.current;
+    let requestedComponent : Set<Calendar.Component> = [
+        Calendar.Component.hour,
+        Calendar.Component.minute,
+        Calendar.Component.second
+    ]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        // Do any additional setup after loading the view, typically from a nib.
+        let timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(timePrinter), userInfo: nil, repeats: true)
+        timer.fire()
     }
     
     override func didReceiveMemoryWarning() {
@@ -20,5 +33,18 @@ class HiderViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    func timeCalculator(dateFormat: String, endTime: String, startTime: Date = Date()) -> DateComponents {
+        formatter.dateFormat = dateFormat
+        let _startTime = startTime
+        let _endTime = formatter.date(from: endTime)
+        
+        let timeDifference = userCleander.dateComponents(requestedComponent, from: _startTime, to: _endTime!)
+        return timeDifference
+    }
+    
+    func timePrinter() -> Void {
+        let time = timeCalculator(dateFormat: "MM/dd/yyyy hh:mm:ss a", endTime: "07/30/2017 04:30:00 a")
+        timeLabel.text = "\(time.minute!) Minutes \(time.second!) Seconds"
+    }
     
 }
