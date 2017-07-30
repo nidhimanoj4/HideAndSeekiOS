de3e4f864c3ec1a67bd5c1e9890ba5bb71e2f61f
carolineh101 <cho19@stanford.edu>
2017-07-29 21:41:40 -0700

    Sets up view controllers

--- Hide-And-Seek/HiderViewController.swift
+++ Hide-And-Seek/HiderViewController.swift
@@ -6,4 +6,19 @@
 //  Copyright © 2017 Nidhi Manoj. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+class HiderViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
